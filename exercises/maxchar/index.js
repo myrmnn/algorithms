0afcca5b86e079150447a808310de88c4dfb93f1
myrmnn <myrm.nn@gmail.com>
2020-06-11 17:52:34 -0400

    completed maxChar exercise

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,29 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+    const charMap = {};
+    let maxChar = '';
+    let max = 0;
+    let myString = str.toLowerCase().replace(/ /g,'');
+
+    for(let char of myString){
+        if(!charMap[char]){
+            charMap[char]  = 1;
+        } else {
+            charMap[char] ++;
+        }
+    }
+
+    for(char in charMap){
+        if(charMap[char] > max){
+            max = char;
+        }
+        maxChar = charMap[char];
+    }
+
+    console.log(max)
+return maxChar;
+}
 
 module.exports = maxChar;
